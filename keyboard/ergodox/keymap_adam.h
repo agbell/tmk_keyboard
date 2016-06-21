616d1c065422afce22fc6a28532c5e6c42d21423
Adam Bell <agbell@gmail.com>
2016-06-21 11:59:14 -0400

    Clean up and addin shift key

--- keyboard/ergodox/keymap_adam.h
+++ keyboard/ergodox/keymap_adam.h
@@ -1,35 +1,10 @@
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * Keymap: Default Layer in QWERTY
-     *
-     * ,--------------------------------------------------.           ,--------------------------------------------------.
-     * |   ~    |   1  |   2  |   3  |   4  |   5  |   \  |           |   -  |   6  |   7  |   8  |   9  |   0  |   =    |
-     * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
-     * | Tab    |   Q  |   W  |   E  |   R  |   T  | ~L5  |           | ~L6  |   Y  |   U  |   I  |   O  |   P  |   [    |
-     * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
-     * | Tab/Shf|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
-     * |--------+------+------+------+------+------|  L0  |           | ~L7  |------+------+------+------+------+--------|
-     * | LCtrl  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   ]    |
-     * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-     *   | ~L5  | ~L2  | Caps | LAlt | LGui |                                       |  Lft |  Up  |  Dn  | Rght | ~L6  |
-     *   `----------------------------------'                                       `----------------------------------'
-     *                                        ,-------------.       ,-------------.
-     *                                        | +L2  | Home |       | PgUp | Del  |
-     *                                 ,------|------|------|       |------+------+------.
-     *                                 |      |      |  End |       | PgDn |      |      |
-     *                                 | BkSp |  ESC |------|       |------| Enter| Space|
-     *                                 |      |      |  Spc |       | Ins  |      |      |
-     *                                 `--------------------'       `--------------------'
-     *
-     *
-     *
-     */
-
+    
     KEYMAP(  // Layer0: default, leftled:none
         // left hand
         FN6,    1,  2,   3,   4,   5,   MINS,
         FN1,    Q,  W,   E,   R,   T,   TAB,
-        FN2,    A,  FN10,   FN8,   FN5, G,
+        FN2,    FN12,  FN10,   FN8,   FN5, G,
         FN4,    Z,  X,   C,   V,   B,   BSPC,
         BSLS,GRAVE,SPC,SPC,SPC,
                                       ESC,LBRC,
@@ -38,7 +13,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
              EQL,    6,   7,   8,   9,   0,   EQL,
              TAB,    Y,   U,   I,   O,   P,   FN0,
-                     H,   FN7, FN9,   FN11,   SCLN,FN3,
+                     H,   FN7, FN9,   FN11,   FN13,FN3,
              DELETE, N,   M,   COMM,DOT, QUOT,FN4,
                        SPC,SPC,  SPC,SPC,SLSH,
         RBRC,ESC,
@@ -126,7 +101,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
                        TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,
-        TRNS,]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+        TRNS,
         TRNS,TRNS,TRNS
     ),
 */
@@ -143,89 +118,22 @@ enum function_id {
 static const uint16_t PROGMEM fn_actions[] = {
     [0] =   ACTION_MODS_TAP_KEY(MOD_RALT, KC_RBRC),            // FN0  - CTRL + J
     [1] =   ACTION_MODS_TAP_KEY(MOD_LALT, KC_LBRC),            // FN1 - CTRL + F
-    [2] =  ACTION_FUNCTION_TAP(LCTL_LPAREN),                   // FN2
-    [3] =  ACTION_FUNCTION_TAP(RCTL_LPAREN),                   // FN3
-    [4] =  ACTION_MODS_ONESHOT(MOD_LSFT),                    // FN4
-    [5] =  ACTION_LAYER_TAP_KEY(1, KC_F),                   // FN5
-    [6] =   ACTION_MODS_TAP_KEY(MOD_LGUI, KC_ESC),            // FN6    
-    [7] =  ACTION_LAYER_TAP_KEY(2, KC_J),                     // FN7  
+    
+    [2] =  ACTION_FUNCTION_TAP(LCTL_LPAREN),                   // FN2 CTRL & Paren
+    [3] =  ACTION_FUNCTION_TAP(RCTL_LPAREN),                   // FN3 CTRL & Paren
+    
+    [4] =  ACTION_MODS_ONESHOT(MOD_LSFT),                      // FN4  One Shot Shift
+    [5] =  ACTION_LAYER_TAP_KEY(1, KC_F),                      // FN5   F Lock Layer 
+    [6] =   ACTION_MODS_TAP_KEY(MOD_LGUI, KC_ESC),             // FN6 Win Key + Esc   
+    [7] =  ACTION_LAYER_TAP_KEY(2, KC_J),                      // FN7  J/F Lock Layer
     [8]   = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_D),
     [9]   = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_K),
     [10]   = ACTION_MODS_TAP_KEY(MOD_LALT, KC_S),   
-    [11]   = ACTION_MODS_TAP_KEY(MOD_RALT, KC_L),   
-    [31] =  ACTION_LAYER_TAP_KEY(1, KC_F),  
+    [11]   = ACTION_MODS_TAP_KEY(MOD_RALT, KC_L),
+    [12]   = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_A),   
+    [13]   = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_SCLN),
 };
 
-
-static const uint16_t PROGMEM fn_actions_ctl [] = {
-   [30] =  ACTION_LAYER_TAP_KEY(4, KC_K), //K return function
-   [31] =  ACTION_LAYER_TAP_KEY(3, KC_D),  // D return function
-   [0] =   ACTION_MODS_KEY(MOD_LCTL, KC_Q),
-   [1] =   ACTION_MODS_KEY(MOD_LCTL, KC_W),
-   [2] =   ACTION_MODS_KEY(MOD_LCTL, KC_E),
-   [3] =   ACTION_MODS_KEY(MOD_LCTL, KC_R),
-   [4] =   ACTION_MODS_KEY(MOD_LCTL, KC_T),
-   [5] =   ACTION_MODS_KEY(MOD_LCTL, KC_Y),
-   [6] =   ACTION_MODS_KEY(MOD_LCTL, KC_U),
-   [7] =   ACTION_MODS_KEY(MOD_LCTL, KC_I),
-   [8] =   ACTION_MODS_KEY(MOD_LCTL, KC_O),
-   [9] =   ACTION_MODS_KEY(MOD_LCTL, KC_P),
-   [10] =   ACTION_MODS_KEY(MOD_LCTL, KC_A),
-   [11] =   ACTION_MODS_KEY(MOD_LCTL, KC_S),
-   [12] =   ACTION_MODS_KEY(MOD_LCTL, KC_D),
-   [13] =   ACTION_MODS_KEY(MOD_LCTL, KC_F),
-   [14] =   ACTION_MODS_KEY(MOD_LCTL, KC_G),
-   [15] =   ACTION_MODS_KEY(MOD_LCTL, KC_H),
-   [16] =   ACTION_MODS_KEY(MOD_LCTL, KC_J),
-   [17] =   ACTION_MODS_KEY(MOD_LCTL, KC_K),
-   [18] =   ACTION_MODS_KEY(MOD_LCTL, KC_L),
-   [19] =   ACTION_MODS_KEY(MOD_LCTL, KC_X),
-   [20] =   ACTION_MODS_KEY(MOD_LCTL, KC_Z),
-   [21] =   ACTION_MODS_KEY(MOD_LCTL, KC_X),
-   [22] =   ACTION_MODS_KEY(MOD_LCTL, KC_C),
-   [23] =   ACTION_MODS_KEY(MOD_LCTL, KC_V),
-   [24] =   ACTION_MODS_KEY(MOD_LCTL, KC_B),
-   [25] =   ACTION_MODS_KEY(MOD_LCTL, KC_N),
-   [26] =   ACTION_MODS_KEY(MOD_LCTL, KC_M),
-   [27] =   ACTION_MODS_KEY(MOD_LCTL, KC_TAB),
-   [28] =   ACTION_MODS_KEY(MOD_LCTL, KC_ENTER),
-   [29] =   ACTION_MODS_KEY(MOD_LCTL, KC_SPACE)
-};
-
-static const uint16_t PROGMEM fn_actions_alt [] = {
-   [30] =  ACTION_LAYER_TAP_KEY(6, KC_L), //L return function
-   [31] =  ACTION_LAYER_TAP_KEY(5, KC_S),  // S return function
-   [0] =   ACTION_MODS_KEY(MOD_LALT, KC_Q),
-   [1] =   ACTION_MODS_KEY(MOD_LALT, KC_W),
-   [2] =   ACTION_MODS_KEY(MOD_LALT, KC_E),
-   [3] =   ACTION_MODS_KEY(MOD_LALT, KC_R),
-   [4] =   ACTION_MODS_KEY(MOD_LALT, KC_T),
-   [5] =   ACTION_MODS_KEY(MOD_LALT, KC_Y),
-   [6] =   ACTION_MODS_KEY(MOD_LALT, KC_U),
-   [7] =   ACTION_MODS_KEY(MOD_LALT, KC_I),
-   [8] =   ACTION_MODS_KEY(MOD_LALT, KC_O),
-   [9] =   ACTION_MODS_KEY(MOD_LALT, KC_P),
-   [10] =   ACTION_MODS_KEY(MOD_LALT, KC_A),
-   [11] =   ACTION_MODS_KEY(MOD_LALT, KC_S),
-   [12] =   ACTION_MODS_KEY(MOD_LALT, KC_D),
-   [13] =   ACTION_MODS_KEY(MOD_LALT, KC_F),
-   [14] =   ACTION_MODS_KEY(MOD_LALT, KC_G),
-   [15] =   ACTION_MODS_KEY(MOD_LALT, KC_H),
-   [16] =   ACTION_MODS_KEY(MOD_LALT, KC_J),
-   [17] =   ACTION_MODS_KEY(MOD_LALT, KC_K),
-   [18] =   ACTION_MODS_KEY(MOD_LALT, KC_L),
-   [19] =   ACTION_MODS_KEY(MOD_LALT, KC_X),
-   [20] =   ACTION_MODS_KEY(MOD_LALT, KC_Z),
-   [21] =   ACTION_MODS_KEY(MOD_LALT, KC_X),
-   [22] =   ACTION_MODS_KEY(MOD_LALT, KC_C),
-   [23] =   ACTION_MODS_KEY(MOD_LALT, KC_V),
-   [24] =   ACTION_MODS_KEY(MOD_LALT, KC_B),
-   [25] =   ACTION_MODS_KEY(MOD_LALT, KC_N),
-   [26] =   ACTION_MODS_KEY(MOD_LALT, KC_M),
-   [27] =   ACTION_MODS_KEY(MOD_LALT, KC_TAB),
-   [28] =   ACTION_MODS_KEY(MOD_LALT, KC_ENTER),
-   [29] =   ACTION_MODS_KEY(MOD_LALT, KC_SPACE)
-};fdfd
 /*
  * user defined action function
  */
@@ -294,8 +202,6 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
 }
 
 #define FN_ACTIONS_SIZE     (sizeof(fn_actions)   / sizeof(fn_actions[0]))
-#define FN_ACTIONS_CTL_SIZE   (sizeof(fn_actions_ctl) / sizeof(fn_actions_ctl[0]))
-#define FN_ACTIONS_ALT_SIZE   (sizeof(fn_actions_alt) / sizeof(fn_actions_alt[0]))
 
 
 /*
@@ -309,18 +215,10 @@ action_t keymap_fn_to_action(uint8_t keycode)
     action_t action;
     action.code = ACTION_NO;
 
-    // CTRL Layer
-    if ((layer == 3 || layer == 4) && FN_INDEX(keycode) < FN_ACTIONS_CTL_SIZE) {
-        action.code = pgm_read_word(&fn_actions_ctl[FN_INDEX(keycode)]);
-    } 
-    // ALT Layer
-    else if ((layer == 5 || layer == 6) && FN_INDEX(keycode) < FN_ACTIONS_ALT_SIZE) {
-        action.code = pgm_read_word(&fn_actions_alt[FN_INDEX(keycode)]);
-    } 
     // by default, use fn_actions from debug_hexdefault layer 0
     // this is needed to get mapping for same key, that was used switch to some layer,
     // to have possibility to switch layers back
-    else if (action.code == ACTION_NO && FN_INDEX(keycode) < FN_ACTIONS_SIZE) {
+    if (action.code == ACTION_NO && FN_INDEX(keycode) < FN_ACTIONS_SIZE) {
         action.code = pgm_read_word(&fn_actions[FN_INDEX(keycode)]);
     }
     debug("FN:\t"); debug_dec(FN_INDEX(keycode));
